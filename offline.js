a471a3968c34b2460b5ddf44d667281deeeca959
babayevstudio <61372267+babayevstudio@users.noreply.github.com>
2020-02-22 22:55:36 -0800

    Ilk oyun
    Beta

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1582439103,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/tiledbackground.png",
+		"images/dino-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"images/stone-sheet0.png",
+		"images/crate-sheet0.png",
+		"images/sprite7-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/star-sheet0.png",
+		"images/spritefont.png",
+		"images/spritefont2.png",
+		"images/spritefont4.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
